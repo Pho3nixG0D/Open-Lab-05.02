5da63577db694307c3030e92184f8722c1cf5820
Pho3nixG0D <oliverkrskagmail.com>
2022-10-09 16:21:53 +0200

    Created while loop, checked if second position from end is ! or ?, then returned substr from sentence -1

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_05._02
 {
@@ -6,7 +7,11 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            while (sentence[sentence.Length - 2] == '!' || sentence[sentence.Length - 2] == '?')
+            {
+                sentence = sentence.Substring(0, sentence.Length - 1);
+            }
+            return sentence;
         }
     }
 }
